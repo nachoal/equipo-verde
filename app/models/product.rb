c69c6205ca7b9d08c17a32660e47f8b24c13fc4a
Ignacio Alonso <ignacio.alley@gmail.com>
2021-08-19 11:17:11 -0500

    Agregar seed file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   has_many :orders, through: :line_items
 
   belongs_to :category
+
+  monetize :price_cents
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Limpiando la DB..."
+Category.destroy_all
+User.destroy_all
+Order.destroy_all
+LineItem.destroy_all
+Product.destroy_all
+
+puts "Creando categorias..."
+maceteros = Category.create!(
+  name: "Maceteros",
+  description: "Dale una mirada distinta a tu jardin"
+)
+
+pastelon = Category.create!(
+  name: "Pastelón",
+  description: "Transforma la entrada de tu hogar y al mismo tiempo impactas al mundo"
+)
+
+limpiapies = Category.create!(
+  name: "Limpiapies",
+  description: "La entrada de la casa con un diseño y producto reciclado"
+)
+
+puts "Creando dos productos para maceteros"
+Product.create!(
+  name: 'macetero verde',
+  conent: 'un macetero verde bonito',
+  price: 20,
+  category: maceteros
+)
+
+puts "Creando dos productos para pastelon"
+producto_pastelon = Product.create!(
+  name: 'pastelon reciclado',
+  conent: 'un pastelon verde bonito',
+  price: 50,
+  category: pastelon
+)
+Product.create!(
+  name: 'pastelon reciclado rojo',
+  conent: 'un pastelon rojo bonito',
+  price: 100,
+  category: pastelon
+)
+
+puts "Creando dos productos para limpiapies"
+Product.create!(
+  name: 'limpiapies reciclado',
+  conent: 'un limpiapies verde bonito',
+  price: 10,
+  category: limpiapies
+)
+producto_limpiapies = Product.create!(
+  name: 'limpiapies reciclado rojo',
+  conent: 'un limpiapies rojo bonito',
+  price: 1000,
+  category: limpiapies
+)
+
+puts "Creando un usuario de prueba"
+usuario = User.create!(email: 'test@test.com', password: '123456')
+
+
+puts "Creando una orden para el usuario de prueba"
+order = Order.create!(
+  user: usuario,
+  status: 'completed',
+  total_price: producto_pastelon.price.fractional + producto_limpiapies.price.fractional
+)
+
+puts "Creando line items para la orden"
+LineItem.create!(
+  order: order,
+  product: producto_pastelon,
+  amount: 1,
+  total_price: producto_pastelon.price.fractional
+)
+
+LineItem.create!(
+  order: order,
+  product: producto_limpiapies,
+  amount: 1,
+  total_price: producto_limpiapies.price.fractional
+)
